2942e8b3c165155e82d6bfef93e31879a2d82478
Y.K <powdersugar828828@gmail.com>
2023-05-31 00:02:56 +0900

    chore: Installed ans set up reg-suit.

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,9 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test.beforeEach(async ({ page }) => {
+  await page.goto('http://localhost:5173');
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
+test('Take a screenshot.', async ({ page }) => {
+  await page.screenshot({ path: './directory_contains_actual_images/example.png', fullPage: true });
 });
